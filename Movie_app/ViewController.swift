bda9fbf11f8ff192f20b64038a0decd0129013aa
reve74 <reve99@naver.com>
2023-10-16 01:34:21 +0900

    이미지 & dateFormat 파싱 적용

--- Movie_app/ViewController.swift
+++ Movie_app/ViewController.swift
@@ -26,6 +26,31 @@ class ViewController: UIViewController {
         requestMovieAPI()
     }
     
+    func loadImage(urlString: String, completion: @escaping (UIImage?)-> Void){
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        if let hasURL = URL(string: urlString) {
+            var request = URLRequest(url: hasURL)
+            request.httpMethod = "GET"
+            
+            session.dataTask(with: request) { data, response, error in
+                print((response as! HTTPURLResponse).statusCode)
+                
+                if let hasData = data {
+                    completion(UIImage(data: hasData))
+                    return
+                }
+            }.resume()
+            session.finishTasksAndInvalidate()
+        }
+        
+        // 실패했을 경우
+        completion(nil)
+    }
+    
+    
+    // api 호출 함수
     func requestMovieAPI() {
         let sessionConfig = URLSessionConfiguration.default
         let session = URLSession(configuration: sessionConfig)
@@ -79,13 +104,24 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return self.movieModel?.results.count ?? 0
     }
     
+    // cell의 높이 지정(디자인 가이드 따라 적용)
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 150
+//    }
+    
+    // 컨텐츠의 내용만큼 높이 지정
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return UITableView.automaticDimension
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         let movieData = self.movieModel?.results[indexPath.row]
         
         cell.titleLabel.text = movieData?.trackName
-        cell.dateLabel.text = movieData?.shortDescription
+        cell.descriptionLabel.text = movieData?.shortDescription
         
         let currency = movieData?.currency ?? ""
         
@@ -93,6 +129,30 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let price = movieData?.trackPrice.description ?? ""
         
         cell.priceLabel.text = currency + price
+        
+        if let hasURL = movieData?.image {
+            self.loadImage(urlString: hasURL) { image in
+                DispatchQueue.main.async {
+                    cell.movieImageView.image = image
+                }
+            }
+        }
+        
+        if let dateString = movieData?.releaseDate {
+            // 2017-05-05T07:00:00Z -> ISO8601 포맷
+            let formatter = ISO8601DateFormatter()
+            if let isoDate = formatter.date(from: dateString) {
+                let myFormater = DateFormatter()
+                myFormater.dateFormat = "yyyy-MM-dd"
+                let dateString = myFormater.string(from: isoDate)
+                
+                cell.dateLabel.text = dateString
+            }
+        }
+        
+        // dateFormat 파싱
+      
+        
         
         return cell
     }
